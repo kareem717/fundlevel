ee35f2bb178ff469e7620926423f0743e5fa1f9e
kareem717 <kareem.717@icloud.com>
2024-09-18 14:54:51 -0400

    refactor(entities): use shared type declaration for account id

--- internal/entities/account/account.go
+++ internal/entities/account/account.go
@@ -6,34 +6,23 @@ import (
 	"github.com/uptrace/bun"
 )
 
-type FirstName struct {
-	FirstName string `json:"firstName" minLength:"3" maxLength:"30" pattern:"^[a-zA-Z]+$" patternDescription:"Must be a alphabetical string with at least 3 characters"`
-}
-
-type LastName struct {
-	LastName string `json:"lastName" minLength:"3" maxLength:"30" pattern:"^[a-zA-Z]+$" patternDescription:"Must be a alphabetical string with at least 3 characters"`
-}
-
 // Account represents an account entity.
 type Account struct {
 	bun.BaseModel `bun:"table:accounts"`
 
-	ID     int       `json:"id"`
-	UserID uuid.UUID `json:"userId" format:"uuid" minLength:"36" maxLength:"36"`
-	FirstName
-	LastName
+	shared.IntegerID
+	CreateAccountParams
 	shared.Timestamps
 }
 
 // CreateAccountParams contains the parameters for creating a new account.
 type CreateAccountParams struct {
-	FirstName
-	LastName
+	UpdateAccountParams
 	UserID uuid.UUID `json:"userId" format:"uuid" minLength:"36" maxLength:"36"`
 }
 
 // UpdateAccountParams contains the parameters for updating a account.
 type UpdateAccountParams struct {
-	FirstName
-	LastName
+	FirstName string `json:"firstName" minLength:"3" maxLength:"30" pattern:"^[a-zA-Z]+$" patternDescription:"Must be a alphabetical string with at least 3 characters"`
+	LastName  string `json:"lastName" minLength:"3" maxLength:"30" pattern:"^[a-zA-Z]+$" patternDescription:"Must be a alphabetical string with at least 3 characters"`
 }
