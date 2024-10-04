abdaa3c61e5a113142b461a7d06126c9a55ac2f0
kareem717 <kareem.717@icloud.com>
2024-10-04 09:22:50 -0400

    feat(components): empty data card

--- apps/web/src/app/(app)/my-rounds/page.tsx
+++ apps/web/src/app/(app)/my-rounds/page.tsx
@@ -1,3 +1,30 @@
+import { EmptySectionCard } from "@/components/app/empty-section-card"
+import { Label } from "@/components/ui/label"
+
 export default async function MyRoundsPage() {
-  return <div>My Rounds</div>
+  return (
+    <div className="p-8 flex flex-col gap-16 justify-center items-center">
+      {Array.from({ length: 3 }).map((_, index) => (
+        <div className="flex flex-col gap-2 w-full" key={index}>
+          <Label className="text-2xl font-bold" htmlFor={`no-rounds-card-${index}`}>
+            Round Type {index + 1}
+          </Label> 
+          <EmptySectionCard
+            id={`no-rounds-card-${index}`}
+            title="No rounds found"
+            description="You don't have any rounds yet. Create one to get started."
+            button={{
+              label: "Create Round",
+              href: "/rounds/create",
+            }}
+            icon="add"
+            image={{
+              src: "/filler.jpeg",
+              alt: "No rounds found",
+            }}
+          />
+        </div>
+      ))}
+    </div>
+  )
 }
