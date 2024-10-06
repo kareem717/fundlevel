3b62ced57c08f7a33a67a7e13ecebbe3df932059
kareem717 <kareem.717@icloud.com>
2024-10-05 23:26:23 -0400

    fix(ui): incorrect mode-toggle icon

--- apps/landing/src/components/mode-toggle.tsx
+++ apps/landing/src/components/mode-toggle.tsx
@@ -22,8 +22,8 @@ export const ModeToggle: FC = () => {
     <Button variant="ghost" size="icon" onClick={toggleTheme}>
       {mounted && (
         resolvedTheme === "light" ?
-          <Icons.sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-foreground" /> :
-          <Icons.moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-foreground" />
+          <Icons.moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-foreground" /> :
+          <Icons.sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-foreground" />
       )}
       <span className="sr-only">Toggle theme</span>
     </Button>
