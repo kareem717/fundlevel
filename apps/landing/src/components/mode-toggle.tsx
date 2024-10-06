47babd6227b3814b6f9c4be0dd58491c95883513
kareem717 <kareem.717@icloud.com>
2024-10-05 23:58:07 -0400

    fix(seo): reduce react in mode-toggle

--- apps/landing/src/components/mode-toggle.tsx
+++ apps/landing/src/components/mode-toggle.tsx
@@ -2,30 +2,21 @@
 
 import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
-import { FC, useEffect, useState } from "react"
+import { FC } from "react"
 import { Icons } from "./icons"
 
 export const ModeToggle: FC = () => {
-  const { setTheme, resolvedTheme } = useTheme()
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
+  const { setTheme, theme } = useTheme()
 
   const toggleTheme = () => {
-    setTheme(resolvedTheme === "light" ? "dark" : "light");
+    setTheme(theme === "light" ? "dark" : "light");
   }
 
-  // Only render the icons after the component has mounted
   return (
-    <Button variant="ghost" size="icon" onClick={toggleTheme}>
-      {mounted && (
-        resolvedTheme === "light" ?
-          <Icons.moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-foreground" /> :
-          <Icons.sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-foreground" />
-      )}
-      <span className="sr-only">Toggle theme</span>
+    <Button variant="ghost" size="icon" onClick={toggleTheme} aria-label="Toggle theme">
+      {/* rendering both icons fixes hydration errors */}
+      <Icons.moon className="size-5 dark:hidden" />
+      <Icons.sun className="size-5 hidden dark:block" />
     </Button>
   )
 }
