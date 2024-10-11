de40f01cac5a8f12f2531db86e1f16d01cc63989
kareem717 <kareem.717@icloud.com>
2024-10-11 12:14:55 -0400

    fix(seo): change icon location

--- apps/landing/src/app/layout.tsx
+++ apps/landing/src/app/layout.tsx
@@ -14,11 +14,11 @@ export default async function RootLayout({
   return (
     <html lang="en" className={GeistSans.className}>
       <Head>
-        <link rel="icon" type="image/png" href="/assets/favicon/favicon-48x48.png" sizes="48x48" />
-        <link rel="icon" type="image/svg+xml" href="/assets/favicon/favicon.svg" />
-        <link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
-        <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body>
         <ThemeProvider
