fa79c4d23435503155953ff15fd7dec678d33cbf
kareem717 <kareem.717@icloud.com>
2024-10-27 12:20:53 -0400

    feat(dashboards): add dynamic breadcrumb

---
+++ apps/web/src/app/(dashboards)/owner/components/dashboard-breadcrumb..tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { ComponentPropsWithoutRef, FC } from "react"
+import { cn } from "@/lib/utils"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import { SidebarTrigger } from "@/components/ui/sidebar"
+import { usePathname } from "next/navigation"
+import { dashboardBreadcrumbConfig } from "@/lib/config/dashboard"
+
+export interface DashboardBreadcrumbProps extends ComponentPropsWithoutRef<"div"> {
+
+};
+
+export const DashboardBreadcrumb: FC<DashboardBreadcrumbProps> = ({ className, ...props }) => {
+  const pathname = usePathname()
+
+  const key = Object.keys(dashboardBreadcrumbConfig).find((key) => pathname.startsWith(key))
+
+  if (!key) {
+    throw new Error(`No title found for pathname: ${pathname}`)
+  }
+
+  //TODO: make this into links
+  return (
+    <div className={cn("flex items-center gap-2 px-4", className)} {...props}>
+      <SidebarTrigger className="-ml-1" />
+      <Separator orientation="vertical" className="mr-2 h-4" />
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem className="hidden md:block">
+            Business Dashboard
+          </BreadcrumbItem>
+          <BreadcrumbSeparator className="hidden md:block" />
+          {dashboardBreadcrumbConfig[key].slice(0, -1).map((item, index) => (  // Ensure correct variable name
+            <BreadcrumbItem key={index} className="hidden md:block">
+              {item}
+            </BreadcrumbItem>
+          ))}
+
+          <BreadcrumbItem key={dashboardBreadcrumbConfig[key].length - 1} >
+            {dashboardBreadcrumbConfig[key][dashboardBreadcrumbConfig[key].length - 1]}
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+};
--- apps/web/src/app/(dashboards)/owner/layout.tsx
+++ apps/web/src/app/(dashboards)/owner/layout.tsx
@@ -19,6 +19,7 @@ import redirects from "@/lib/config/redirects";
 import AuthProvider from "@/components/providers/auth-provider";
 import { getAccount, getUser } from "@/actions/auth";
 import { getAccountBusinesses } from "@/actions/busineses"
+import { DashboardBreadcrumb } from "./components/dashboard-breadcrumb."
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -50,23 +51,7 @@ export default async function DashboardLayout({
         <AppSidebar businesses={businessesResponse.data.businesses} />
         <SidebarInset>
           <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-            <div className="flex items-center gap-2 px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem className="hidden md:block">
-                    <BreadcrumbLink href="#">
-                      Building Your Application
-                    </BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator className="hidden md:block" />
-                  <BreadcrumbItem>
-                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
-            </div>
+            <DashboardBreadcrumb />
           </header>
           <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
             {children}
