afe77c71738125cbc54ffcfc9348e43d9045a1fd
ForrestDevs <luke.gannon@me.com>
2024-11-04 13:00:04 -0500

    chore: fix openapi config

--- apps/web/openapi-ts.config.ts
+++ apps/web/openapi-ts.config.ts
@@ -1,15 +1,9 @@
 import { env } from "./src/env";
 import { defineConfig } from "@hey-api/openapi-ts";
-import * as dotenv from 'dotenv'
-
-dotenv.config({
-	path: ".env.local",
-});
 
 export default defineConfig({
 	client: "@hey-api/client-fetch",
-	input: process.env.NEXT_PUBLIC_BACKEND_OPENAPI_DOCS_URL!,
-	// input: env.NEXT_PUBLIC_BACKEND_OPENAPI_DOCS_URL,
+	input: env.NEXT_PUBLIC_BACKEND_OPENAPI_DOCS_URL,
 	output: {
 		format: "prettier",
 		lint: "eslint",
