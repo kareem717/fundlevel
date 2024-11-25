f041c58ec6461141700e63248d662dfa35b42797
ForrestDevs <luke.gannon@me.com>
2024-11-24 23:22:54 -0500

    chore: fix small layout bugs

--- apps/landing/src/app/(frontend)/page.tsx
+++ apps/landing/src/app/(frontend)/page.tsx
@@ -16,38 +16,15 @@ export default async function Home() {
   } = copy;
 
   return (
-    <div className="p-4 flex flex-col justify-between h-[300dvh] w-full max-w-screen-2xl mx-auto items-center relative">
+    <div className="flex flex-col w-full items-center relative gap-4">
       <NavBar
         config={navigationConfig}
         currentPath={"/"}
-        className="sticky top-0 z-50"
+        className="sticky top-0 z-50 border-b w-full"
       />
-      <Hero
-        id="hero"
-        className="pt-8 md:pt-56 lg:pt-64 w-full h-[95dvh] md:h-[70vh] xl:h-[1028px]"
-      />
-      <div className="flex flex-col items-center justify-center gap-4 sm:gap-8">
-        <h1 className="text-3xl sm:text-4xl font-bold text-center">
-          The right way to <span className="text-primary">fund</span>.
-        </h1>
-        <div className="flex flex-col gap-4 w-2/3">
-          <Link
-            href={contact.calendly}
-            className={cn("uppercase font-bold w-full", buttonVariants())}
-          >
-            {hero.meetingCTA}
-            <Icons.arrowRight className="size-4 ml-2" />
-          </Link>
-          <Link
-            href={hero.newsletter.signUpURL}
-            className={cn(
-              "uppercase font-bold w-full",
-              buttonVariants({ variant: "secondary" })
-            )}
-          >
-            {hero.newsletter.CTA}
-          </Link>
-        </div>
+      <Hero id="hero" className="w-full py-4 h-[95dvh]" />
+
+      <div className="flex flex-col items-center justify-center mx-auto px-4 gap-4 sm:gap-8">
         <BentoDemo />
         <Newsletter />
       </div>
