154f251138773d7dd96f611d37be67ab0ae558fe
ForrestDevs <luke.gannon@me.com>
2024-12-01 18:04:44 -0500

    feat: update investors problem statement

--- apps/landing/src/components/routes/investors/problem.tsx
+++ apps/landing/src/components/routes/investors/problem.tsx
@@ -1,56 +1,83 @@
-import { FC } from "react";
-import { buttonVariants } from "@/components/ui/button";
-import Link from "next/link";
-import { cn } from "@/lib/utils";
+import { FC } from 'react'
+import { cn } from '@/lib/utils'
+import { Container, Section } from '@/components/layout'
+import Balancer from 'react-wrap-balancer'
 
 interface ProblemStatementProps {
-  className?: string;
+  className?: string
 }
 
 const ProblemStatement: FC<ProblemStatementProps> = ({ className }) => {
+  const stats = [
+    {
+      stat: '$10M',
+      title: 'Minimum investment',
+      description:
+        'Many commercial private equity funds require immense capital investments to even work with them - barring most from reaping the gains in the growing market.',
+    },
+    {
+      stat: '34.5M',
+      title: 'Businesses in the US & Canada',
+      description:
+        'Out of the entire 34.5 million businesses, over 90% are considered small to medium.',
+    },
+    {
+      stat: '44%',
+      title: 'Businesses fail due to lack of capital',
+      description:
+        'Even when businesses have huge potential, simply running out of cash can destroy them.',
+    },
+  ]
+
   return (
-    <section className={cn("container py-16 space-y-8 bg-muted/50", className)}>
-      <div className="space-y-4 text-center max-w-3xl mx-auto">
-        <h2 className="text-3xl font-bold">
-          Private markets have outstanding potential but are inaccessible or
-          non-compliant.
-        </h2>
-        <p className="text-lg text-muted-foreground">
-          There is over <span className="font-bold">$13 trillion</span> under
-          management in private market investments, though very few are able to
-          find opportunities for themselves - let alone while being shariah
-          compliant.
-        </p>
-      </div>
+    <Section className={cn('', className)}>
+      <Container>
+        <div className="space-y-6 text-center max-w-3xl mx-auto">
+          <h2 className="text-3xl font-medium leading-tight tracking-tight sm:text-4xl md:text-5xl">
+            <Balancer>
+              Private markets have{' '}
+              <span className="bg-gradient-to-r from-orange-400 to-rose-400 bg-clip-text text-transparent">
+                outstanding potential
+              </span>{' '}
+              but are inaccessible or non-compliant.
+            </Balancer>
+          </h2>
+          <p className="text-lg text-muted-foreground sm:text-xl">
+            <Balancer>
+              There is over <span className="font-medium">$13 trillion</span>{' '}
+              under management in private market investments, though very few
+              are able to find opportunities for themselves - let alone while
+              being shariah compliant.
+            </Balancer>
+          </p>
+        </div>
 
-      <div className="grid md:grid-cols-3 gap-8">
-        <div className="space-y-4">
-          <h3 className="text-2xl font-bold">$10M</h3>
-          <p className="text-muted-foreground">
-            Many commercial private equity funds require immense capital
-            investments to even work with them - barring most from reaping the
-            gains in the growing market.
-          </p>
+        <div className="grid md:grid-cols-3 gap-8">
+          {stats.map((stat) => (
+            <StatCard key={stat.title} {...stat} />
+          ))}
         </div>
-        <div className="space-y-4">
-          <h3 className="text-2xl font-bold">34.5M</h3>
-          <p className="text-muted-foreground">
-            Out of the entire 34.5 million businesses, over 90% are considered
-            small to medium. These businesses are not easily able to raise funds
-            outside of debt financing - even when they are insanely profitable.
-          </p>
-        </div>
-        <div className="space-y-4">
-          <h3 className="text-2xl font-bold">44%</h3>
-          <p className="text-muted-foreground">
-            Even when businesses have huge potential, simply running out of cash
-            can destroy them. When pockets are thin, it&apos;s time for
-            investors to partner with businesses - rewarding both parties.
-          </p>
-        </div>
-      </div>
-    </section>
-  );
-};
+      </Container>
+    </Section>
+  )
+}
 
-export { ProblemStatement };
+interface StatCardProps {
+  stat: string
+  title: string
+  description: string
+}
+
+const StatCard: FC<StatCardProps> = ({ stat, title, description }) => {
+  return (
+    <div className="space-y-4 p-6 rounded-lg bg-secondary">
+      <h4 className="text-3xl font-medium bg-gradient-to-r from-orange-400 to-rose-400 bg-clip-text text-transparent">
+        {stat}
+      </h4>
+      <h3>{title}</h3>
+      <p className="text-muted-foreground">{description}</p>
+    </div>
+  )
+}
+
+export { ProblemStatement }
