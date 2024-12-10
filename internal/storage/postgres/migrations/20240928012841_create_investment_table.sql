c044524ce1af8448b7b1da45cae34342674df899
Kareem Yakubu <kareem.717@icloud.com>
2024-12-10 11:42:50 -0500

    feat: add investment payment entity

--- internal/storage/postgres/migrations/20240928012841_create_investment_table.sql
+++ internal/storage/postgres/migrations/20240928012841_create_investment_table.sql
@@ -31,36 +31,9 @@ CREATE TRIGGER sync_investments_updated_at BEFORE
 UPDATE ON investments FOR EACH ROW
 EXECUTE PROCEDURE sync_updated_at_column ();
 
--- CREATE TYPE payment_status AS ENUM(
---     'cancelled',
---     'processing',
---     'requires_action',
---     'requires_capture',
---     'requires_confirmation',
---     'requires_payment_method',
---     'succeeded'
--- );
--- CREATE TABLE
---     investment_payments (
---         investment_id INT NOT NULL REFERENCES investments (id) PRIMARY KEY,
---         stripe_payment_intent_id TEXT NOT NULL,
---         stripe_payment_intent_client_secret TEXT NOT NULL,
---         status payment_status DEFAULT 'processing',
---         created_at timestamptz DEFAULT CLOCK_TIMESTAMP(),
---         updated_at timestamptz,
---         deleted_at timestamptz
---     );
--- CREATE UNIQUE INDEX ON investment_payments (stripe_payment_intent_id)
--- WHERE
---     deleted_at IS NULL;
--- CREATE TRIGGER sync_investment_payments_updated_at BEFORE
--- UPDATE ON investment_payments FOR EACH ROW
--- EXECUTE PROCEDURE sync_updated_at_column ();
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
--- DROP TABLE investment_payments;
--- DROP TYPE payment_status;
 DROP TABLE investment;
 
 DROP TYPE investment_status;
---
+++ internal/storage/postgres/migrations/20240928012842_create_investment_payments_table.sql
@@ -0,0 +1,44 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TYPE stripe_payment_intent_status AS ENUM(
+    'cancelled',
+    'processing',
+    'requires_action',
+    'requires_capture',
+    'requires_confirmation',
+    'requires_payment_method',
+    'succeeded'
+);
+
+CREATE TABLE
+    investment_payments (
+        id SERIAL PRIMARY KEY,
+        investment_id INT NOT NULL REFERENCES investments (id),
+        stripe_payment_intent_id TEXT NOT NULL,
+        stripe_payment_intent_client_secret TEXT NOT NULL,
+        status stripe_payment_intent_status DEFAULT 'processing',
+        created_at timestamptz DEFAULT CLOCK_TIMESTAMP(),
+        updated_at timestamptz,
+        deleted_at timestamptz
+    );
+
+CREATE TRIGGER sync_investment_payments_updated_at BEFORE
+UPDATE ON investment_payments FOR EACH ROW
+EXECUTE PROCEDURE sync_updated_at_column ();
+
+CREATE UNIQUE INDEX ON investment_payments (stripe_payment_intent_id)
+WHERE
+    deleted_at IS NULL;
+
+CREATE UNIQUE INDEX ON investment_payments (investment_id)
+WHERE
+    deleted_at IS NULL;
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE investment_payments;
+
+DROP TYPE stripe_payment_intent_status;
+
+-- +goose StatementEnd
