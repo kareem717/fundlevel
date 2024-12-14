1eddeb189af51053a786821095b49cdadb1f0cd9
Kareem Yakubu <kareem.717@icloud.com>
2024-12-13 22:49:46 -0500

    feat: hide unready pages

---
+++ apps/landing/src/app/(main)/_hidden/businesses/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from '../../../../components/routes/businesses/hero'
+import Problem from '../../../../components/routes/businesses/problem'
+import Value from '../../../../components/routes/businesses/value'
+import Models from '../../../../components/routes/businesses/models'
+import { StandoutSection } from '../../../../components/routes/businesses/standout'
+import { CTA } from '../../../../components/routes/businesses/cta'
+import FAQ from '../../../../components/routes/businesses/faq'
+
+export default function Businesses() {
+  return (
+    <main className="relative flex flex-col items-center w-full">
+      <Hero />
+      <Problem />
+      <Value />
+      <Models />
+      <StandoutSection />
+      <CTA />
+      <FAQ />
+    </main>
+  )
+}
