6cf1e00e1b0d6cd5efeab33615a11c0d39a34b46
Kareem Yakubu <kareem.717@icloud.com>
2025-01-04 22:51:13 -0500

    refactor(landing): remove newsletter component

--- apps/landing/src/app/page.tsx
+++ apps/landing/src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Hero } from "@/app/components/hero";
-import { Newsletter } from "@/app/components/newsletter";
 import { BentoFeatures } from "@/app/components/bento";
 import { LandingSection } from "./components/landing-section";
 import { CTACard } from "./components/cta-card";
 import { copy } from "@/lib/config/copy";
 import { NumberedCard } from "./components/numbered-card";
+import { Mail } from "lucide-react";
+import { NewsletterSubscribeForm } from "@/components/newsletter-subscribe-form";
 
 export default async function Home() {
   const { ctas, models } = copy.landing;
@@ -41,7 +42,31 @@ export default async function Home() {
           ))}
         </div>
       </LandingSection>
-      <Newsletter />
+      <LandingSection>
+        <div
+          className="grid grid-cols-1 md:grid-cols-2 gap-6 px-6 py-12 rounded-lg sm:px-8 bg-secondary"
+        >
+          <div className="flex flex-col items-start gap-4 md:flex-row">
+            <div className="flex items-center justify-center flex-shrink-0 w-10 h-10 rounded-lg bg-foreground">
+              <Mail className="w-6 h-6 text-background" />
+            </div>
+            <div className="flex flex-col items-start gap-2">
+              <h2 className="font-sans text-3xl font-medium text-left">
+                Keep up with the latest
+              </h2>
+              <p className="text-muted-foreground">
+                Subscribe to our newsletter to get the latest Fundlevel news.
+              </p>
+            </div>
+          </div>
+          <div className="space-y-3 w-full">
+            <h3 className="hidden text-sm font-medium text-gray-600 dark:text-gray-300 lg:block">
+              Subscribe to our newsletter
+            </h3>
+            <NewsletterSubscribeForm className="flex flex-row gap-2" inputClassName="bg-background" />
+          </div>
+        </div>
+      </LandingSection>
     </div>
   );
 }
