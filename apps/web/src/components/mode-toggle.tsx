d337e1542ecdcc52a6214d185e0695a8a28ca878
Kareem Yakubu <kareem.717@icloud.com>
2025-01-24 18:47:56 -0500

    feat(web): swap to large mode toggle

--- apps/web/src/components/mode-toggle.tsx
+++ apps/web/src/components/mode-toggle.tsx
@@ -1,32 +1,22 @@
 "use client"
 
 import { useTheme } from "next-themes"
-import { Button } from "@repo/ui/components/button"
-import { ComponentPropsWithoutRef, FC } from "react"
-import { Icons } from "./icons"
+import { ComponentPropsWithoutRef } from "react"
+import { Sun, Moon } from "lucide-react"
+import { ToggleGroup, ToggleGroupItem } from "@repo/ui/components/toggle-group"
+import { cn } from "@repo/ui/lib/utils"
 
-export interface ModeToggleProps extends ComponentPropsWithoutRef<typeof Button> {
-
-}
-
-export const ModeToggle: FC<ModeToggleProps> = ({ ...props }) => {
-  const { setTheme, theme } = useTheme()
-
-  const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light");
-  }
+export function ModeToggle({className, ...props }: Omit<ComponentPropsWithoutRef<typeof ToggleGroup>, "defaultValue" | "onValueChange" | "value" | "type">) {
+  const { setTheme, resolvedTheme } = useTheme()
 
   return (
-    <Button
-      variant="ghost"
-      size="icon"
-      onClick={toggleTheme}
-      aria-label="Toggle theme"
-      {...props}
-    >
-      {/* rendering both icons fixes hydration errors */}
-      <Icons.moon className="size-5 dark:hidden" />
-      <Icons.sun className="size-5 hidden dark:block" />
-    </Button>
+    <ToggleGroup {...props} type="single" onValueChange={setTheme} value={resolvedTheme} className={cn("[&>*]:w-full", className)}>
+      <ToggleGroupItem value="light" aria-label="Toggle light">
+        <Sun className="h-4 w-4" />
+      </ToggleGroupItem>
+      <ToggleGroupItem value="dark" aria-label="Toggle dark">
+        <Moon className="h-4 w-4" />
+      </ToggleGroupItem>
+    </ToggleGroup>
   )
 }
