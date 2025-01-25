af0fd45dab27b30fa9cb8842782da91960b5d598
Kareem Yakubu <kareem.717@icloud.com>
2025-01-25 15:04:26 -0500

    refactor(web): move theme-provider

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -5,7 +5,7 @@ import { env } from "@/env";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google"
 import { NuqsAdapter } from "nuqs/adapters/next/app";
-import { ThemeProvider } from "@/app/components/theme-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import { Toaster } from "@repo/ui/components/toaster"
 
 export const metadata: Metadata = {
