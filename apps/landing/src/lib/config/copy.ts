1be8ef1f2c5aba1e36200a6a1d6cbf4b5d033527
Kareem Yakubu <kareem.717@icloud.com>
2025-04-22 11:54:11 -0400

    feat(landing): add copy

--- apps/landing/src/lib/config/copy.ts
+++ apps/landing/src/lib/config/copy.ts
@@ -1,6 +1,6 @@
 export const copy = {
   hero: {
-    main: "Stuff",
-    subheading: "stuff2"
+    main: "Operational solutions for finance teams",
+    subheading: "AI tooling made to increase the throughput of financal operators. Empowering your team, not replacing them."
   }
 }
